25a8fad05511b81b66c6c7787d9a7412abb78cca
WesleyAlfaro <wesleyvaldini@gmail.com>
2023-12-16 00:25:06 -0600

    Commit para conexion a base de datos

---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+// Representacion de lo que vamos a grabar en BD, TABLA
+@Schema()
+export class Pokemon extends Document{
+    //id: string // mongo me lo da
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    nombre: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon );
